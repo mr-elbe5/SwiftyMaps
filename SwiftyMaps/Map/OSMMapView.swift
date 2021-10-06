9222fd8c0fa0b50ad3427b8781b98d552e080e11
mr-elbe5 <miro@elbe5.de>
2021-10-06 16:14:57 +0200

    calculate tiles and load

---
+++ SwiftyMaps/Map/OSMMapView.swift
@@ -0,0 +1,20 @@
+//
+//  OSMMapView.swift
+//  SwiftyMaps
+//
+//  Created by Michael Rönnau on 05.10.21.
+//
+
+import Foundation
+import UIKit
+import CoreLocation
+
+class OSMMapView : UIView{
+    
+    var zoom : Int = 8
+    var centerCoordinate = CLLocation()
+    var tileGrid = OSMTileGrid()
+    
+    
+    
+}
